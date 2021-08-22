1f6c7307b67c83907169ef8c83760cfefb051365
PavlovIS777 <p.makeev@yahoo.com>
2021-08-22 21:58:58 +0300

    ver3

--- 0.SqrEquation/main.c
+++ 0.SqrEquation/main.c
@@ -1,53 +1,144 @@
 #include <stdio.h>
-
+#include <math.h>
+#include <stdlib.h>
 
 //binRoot documentation.
 //type: function.
 //description: double function that's finding root from number using bin search method.
 //             Returns double root from number.
 //example: binRoot(4), returns 2.
-double binRoot(double d){
-    double start = 0;
-    double end = d;
-    double mid = (end + start) / 2;
-    const double precision = 1e-9;
-
-    while (end - start > precision){
-        if (mid * mid > d){
-            end = mid;
-            mid = (end + start) / 2;
-        } else {
-            start = mid;
-            mid = (end + start) / 2;
+int isRootsReal(double *d)
+{
+    if ((*d) < 0){
+        printf("Roots are complex!\n");
+        (*d) = -*d;
+        (*d) = sqrt((*d));
+        return 0;
+    } else {
+        if ((*d) == 0)
+        {
+            printf("Root is real!\n");
+            return 1;
+        }
+        else
+        {
+            (*d) = sqrt((*d));
+            printf("Roots are real!\n");
+            return 2;
         }
     }
-    return mid;
+}
+
+void LinSolution(double *a, double *b)
+{
+    if ((*b) == 0 && (*a) == 0)
+    {
+        printf("Root: x belongs R");
+        return;
+    }
+    else if ((*a) == 0)
+    {
+        printf("Root doesn't exist :^(");
+        return;
+    }
+    else
+    {
+        printf("Root: x = %lf!", -(*b)/(*a));
+        return;
+    }
+}
+
+int greeting(void){
+    printf("Hello, user! Please, choose which equation you would like to solve\n Type \"s\" for square and \"l\" "
+           "for linear.\n");
+    char type = ' ';
+    do
+    {
+        scanf("%c", &type);
+        if (type != 's' && type != 'l')
+        {
+            printf("Type \"s\" for square and \"l\" for linear.\n");
+        }
+    } while (type != 's' && type != 'l');
+
+    if (type == 's')
+    {
+        printf("Please, enter the coefficients.\nExample: For equation x^2 + 2.1x + 1 = 0\n"
+               "Write: 1 2.1 1\n");
+        return 0;
+    }
+    else
+    {
+        printf("Please, enter the coefficients.\nExample: For equation x + 1 = 0\n"
+               "Write: 1 1\n");
+        return 1;
+    }
+}
+
+int getNumbersSqr(double *a, double *b, double *c)
+{
+    while(scanf("%lf %lf %lf", a, b, c) != 3)
+    {
+        while(fgetc(stdin) != '\n');
+        printf ("Wrong input, try again.\n");
+    };
+}
+
+int getNumbersLin(double *a, double *b)
+{
+    while(scanf("%lf %lf", a, b) != 2)
+    {
+        while(fgetc(stdin) != '\n');
+        printf ("Wrong input, try again.\n");
+    };
+}
+
+int sqrSolution(double *a, double *b, double *c)
+{
+    double *disc = malloc(sizeof(double));
+    (*disc) = (*b) * (*b) - 4 * (*a) * (*c);
+    if ((*a) == 0 && (*b) == 0 && (*c) == 0)
+    {
+        printf("x belongs R");
+        return 0;
+    }
+    if ((*a) == 0)
+    {
+        LinSolution(b, c);
+    }
+    int condition = isRootsReal(disc);
+    if(condition == 2)
+    {
+        printf("Roots: x1 = %lf and x2 = %lf!",(-(*b)-(*disc))/(2*(*a)), (-(*b)+(*disc))/(2*(*a)));
+    }
+    else if (condition == 1)
+    {
+        printf("Root: x = %lf!", -(*b)/(2*(*a)));
+    }
+    else
+    {
+        printf("Roots: x1 = %lf - %lf""i and x2 = %lf + %lf""i", -(*b)/(2*(*a)), (*disc)/(2*(*a)), -(*b)/(2*(*a)), (*disc)/(2*(*a)));
+    }
+    return 1;
+}
+
+void interface(double *a, double *b, double *c)
+{
+    if(greeting() == 1)
+    {
+        getNumbersLin(a, b);
+        (*c) = 0;
+        LinSolution(a, b);
+    }
+    else
+    {
+        getNumbersSqr(a, b, c);
+        sqrSolution(a, b, c);
+    }
 }
 
 int main() {
-    double a = 0, b = 0, c = 0, d;
-    _Bool real = 1;
-    printf("Hello, user! Please, enter the coefficients.\nExample: For equation x^2 + 2x + 1 = 0\nWrite: 1 2 1.\n");
-    scanf("%lf %lf %lf", &a, &b, &c);
-    d = b*b - 4 * a * c;
-    if (d < 0){
-        real = 0;
-        printf("Roots are complex!\n");
-        d = -d;
-    } else {
-        printf("Roots are real!\n");
-    }
-    d = binRoot(d);
-    if (real){
-        double x1 = (-b - d)/(2 * a);
-        double x2 = (-b + d)/(2 * a);
-        if (x1 != x2)
-            printf("Real Roots: x1 = %lf and x2 = %lf", x1, x2);
-        else
-            printf("Real Root: x = %lf", x1);
-    } else {
-        printf("Complex Roots: x1 = %lf - %lf" "i and x2 = %lf + %lf" "i", -b/(2 * a), d/(2 * a), -b/(2 * a), d/(2 * a));
-    }
-    getchar();
+    double a = 0, b = 0, c = 0;
+    interface(&a, &b, &c);
     return 0;
 }
