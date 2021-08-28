d622047c8da1d0da6b7e642ee07380a4e1d3a6a2
PavlovIS777 <p.makeev@yahoo.com>
2021-08-28 19:15:09 +0300

    devoloping

--- 1.StrLib/StrLib.cpp
+++ 1.StrLib/StrLib.cpp
@@ -1,14 +1,13 @@
 #include "StrLib.h"
 
-int customPuts(const char* str)
+int customPuts(const char* string)
 {
-    if (str == nullptr)
-        return EOF; //код ошибки енам.
+    if (string == nullptr)
+        return EOF;
     size_t symbolPos = 0;
-    while (str[symbolPos])
+    for(size_t symbolPos = 0; string[symbolPos]; ++symbolPos)
     {
-        fputc(str[symbolPos], stdout);
-        ++symbolPos;
+        fputc(string[symbolPos], stdout);
     }
     fputc('\n', stdout);
     return 1;
@@ -16,14 +15,14 @@ int customPuts(const char* str)
 
 char* customStrchr(char* string, char symbol)
 {
-    size_t symbolPos = 0;
+    
 
-    while (string[symbolPos])
+    for(size_t symbolPos = 0; string[symbolPos]; ++symbolPos)
     {
         if (string[symbolPos] == symbol) {
             return string + symbolPos;
         }
-        ++symbolPos;
+        
     }
 
     return nullptr;
@@ -41,13 +40,94 @@ size_t customStrlen(const char* str)
     return symbolPos;
 }
 
-char* customStrcopy(const char* strToCopy, char* strToPaste)
+int customStrcpy(const char* strToCopy, char* strToPaste)
 {
-    size_t symbolPos = 0;
-    while (strToCopy[symbolPos])
+    if (sizeof(*strToCopy > *strToPaste))
+        return EOF;
+    while (*strToCopy != 0)
     {
-        strToPaste[symbolPos] = strToCopy[symbolPos];
+        *strToPaste = *strToCopy;
+        ++strToCopy;
+        ++strToPaste;
     }
 
+    return 1;
 }
 
+void customStrncpy(const char* strToCopy, char* strToPaste, int n)
+{   
+    while (*strToCopy != 0 && *strToPaste != 0 && n)
+    {
+        *strToPaste = *strToCopy;
+        --n;
+        ++strToPaste;
+        ++strToCopy;
+    }
+    
+    return;
+}
+
+void customStrcat(char* destination, char* srcstr)
+{
+    while(*destination)
+    {
+        ++destination;
+    }
+
+    do
+    {
+        *destination = *srcstr;
+        ++destination;
+        ++srcstr;
+    } while (*srcstr);
+}
+
+void customStrncat(char* destination, char* srcstr, int n)
+{
+    while (*destination)
+    {
+        ++destination;
+    }
+
+    do
+    {
+        --n;
+        *destination = *srcstr;
+        ++destination;
+        ++srcstr;
+    } while (*srcstr && n);
+}
+
+int customFgets(FILE* fp, int MAXLEN, char* string)
+{
+    if (fp == NULL){
+        return EOF;
+    }
+    char temp;
+    do
+    {
+        temp = fgetc(fp);
+        *string = temp;
+        ++string;
+        --MAXLEN;
+    } while (temp != '\n' && temp != EOF  && MAXLEN > 1 );
+
+    return 1;
+}
+
+char* customStrdup(char* string)
+{
+    if (string == NULL)
+        return nullptr;
+    
+    char* copy = (char*)malloc(sizeof(*string)+sizeof(char));
+
+    while (*string)
+    {
+        *copy = *string;
+        ++copy;
+        ++string;
+    }
+
+    return copy;
+}
--- 1.StrLib/StrLib.h
+++ 1.StrLib/StrLib.h
@@ -3,6 +3,7 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int customPuts(const char* str);
 
@@ -10,5 +11,15 @@ char* customStrchr(const char* string, char symbol);
 
 size_t customStrlen(const char* str);
 
-char* customStrcopy(const char* strToCopy, char* strToPaste);
+int customStrcpy(const char* strToCopy, char* strToPaste);
+
+void customStrncpy(const char* strToCopy, char* strToPaste, int n);
+
+void customStrcat(char* destination, char* srcstr);
+
+void customStrncat(char* destination, char* srcstr, int n);
+
+int customFgets(FILE* fp, int MAXLEN, char* string);
+
+char* customStrdup(char* string);
 #endif //STRLIB_H
