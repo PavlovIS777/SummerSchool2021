db7d737db033926b8500d92af9f8195d3a956478
PavlovIS777 <p.makeev@yahoo.com>
2021-08-28 12:00:12 +0300

    modified:   .gitignore

--- copy/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "StrLib.h"
-//#include <stdio.h>
-int main(void)
-{
-    const char str[] = "3132";
-    char* searchChar = strchrCustom(str, '1');
-    printf("%c", *searchChar);
-    return 1;
-}
