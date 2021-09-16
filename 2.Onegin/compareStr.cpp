ed8d0774778dd114f4533668b4c254f95c530b24
PavlovIS777 <89222677+PavlovIS777@users.noreply.github.com>
2021-09-16 22:59:01 +0300

    Delete compareStr.cpp

--- 2.Onegin/compareStr.cpp
+++
@@ -1,9 +0,0 @@
-#include "compareStr.h"
-#include <string.h>
-
-int compareStr(const void* s_void, const void* t_void)
-{
-    char** s = (char**) s_void;
-    char** t = (char**) t_void;
-    return strcmp(*s, *t);
-}
